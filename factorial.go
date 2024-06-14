7ec4f98baf074c668fc9b234d230a21bd8dd1cfc
Vitaliy <unstvi@gmail.com>
2024-06-14 13:09:00 +0300

    tiny fix

--- factorial.go
+++ factorial.go
@@ -5,9 +5,10 @@ import (
 )
 
 func factorial(n int) *big.Int {
-	result := big.NewInt(1)
-	for i := 2; i <= n; i++ {
-		result.Mul(result, big.NewInt(int64(i)))
+	if n <= 1 {
+		return big.NewInt(1)
 	}
-	return result
+	previousFactorial := factorial(n - 1)
+	current := big.NewInt(int64(n))
+	return current.Mul(current, previousFactorial)
 }
